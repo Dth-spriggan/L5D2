8e95a5e2c0dad0e9f368f13b735d2225e6815ae1
haicter <dhai29047@gmail.com>
2026-03-06 14:26:26 +0700

    user update

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.ENUM('Candidate', 'Employer'), allowNull: false },
+    role: { type: DataTypes.ENUM('Candidate', 'Employer', 'BLV XoiLacTV'), allowNull: false },
     fullName: { type: DataTypes.STRING },
     phone: { type: DataTypes.STRING },
     kybDocument: { type: DataTypes.STRING },
